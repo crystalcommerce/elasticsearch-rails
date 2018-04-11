ac526b1eab62b6ec5b99310a6a762ea285567f62
Marcelo Ribeiro <marcelo.ribeiro@crystalcommerce.com>
2018-04-10 18:13:13 -0700

    Custom es index

--- elasticsearch-model/lib/elasticsearch/model/adapters/mongoid.rb
+++ elasticsearch-model/lib/elasticsearch/model/adapters/mongoid.rb
@@ -64,14 +64,21 @@ module Elasticsearch
           #
           def __find_in_batches(options={}, &block)
             options[:batch_size] ||= 1_000
-  
-            all.no_timeout.each_slice(options[:batch_size]) do |items|
+
+            s = all
+            if options[:user_collection].present?
+              s = from_collection(options[:user_collection])
+            end
+            s.no_timeout.each_slice(options[:batch_size]) do |items|
               yield items
             end
           end
 
           def __transform
-            lambda {|a|  { index: { _id: a.id.to_s, data: a.as_indexed_json } }}
+            lambda {|a|  { index: {
+              _id: a.respond_to?(:es_id) && a.es_id.present? ? a.es_id : a.id.to_s,
+              data: a.as_indexed_json
+            } }}
           end
         end
 
